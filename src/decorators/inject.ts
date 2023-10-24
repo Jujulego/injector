15739645c609c453c6cf272cfa0704ac5b016502
Julien Capellari <julien.capellari@gmail.com>
2023-10-25 01:02:52 +0200

    feat: inject support accessors

--- src/decorators/inject.ts
+++ src/decorators/inject.ts
@@ -2,9 +2,20 @@ import { InjectableType } from '../defs/index.js';
 
 // Types
 export type InjectInitializer<I> = () => I;
-export type InjectDecorator<I> = <T>(target: undefined, ctx: ClassFieldDecoratorContext<T, I>) => InjectInitializer<I>;
+
+export type InjectAccessorDecorator<I> = <T>(target: ClassAccessorDecoratorTarget<T, I>, ctx: ClassAccessorDecoratorContext<T, I>) => ClassAccessorDecoratorResult<T, I>;
+export type InjectFieldDecorator<I> = <T>(target: undefined, ctx: ClassFieldDecoratorContext<T, I>) => InjectInitializer<I>;
+export type InjectDecorator<I> = InjectAccessorDecorator<I> | InjectFieldDecorator<I>;
 
 // Decorator
 export function Inject<I>(cls: InjectableType<I>): InjectDecorator<I> {
-  return () => () => new cls();
+  return <InjectDecorator<I>>((_target: unknown, ctx: ClassAccessorDecoratorContext<unknown, I> | ClassFieldDecoratorContext<unknown, I>) => {
+    if (ctx.kind === 'field') {
+      return () => new cls();
+    } else {
+      return {
+        init: () => new cls(),
+      };
+    }
+  });
 }
