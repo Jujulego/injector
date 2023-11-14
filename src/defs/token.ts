9d0451497178e0ed3bbe362a81569cadd48083c5
Julien Capellari <julien.capellari@gmail.com>
2023-11-14 20:01:40 +0100

    feat: make override return given value

--- src/defs/token.ts
+++ src/defs/token.ts
@@ -1,5 +1,8 @@
 import { AsyncRef, Ref, SyncRef } from '@jujulego/aegis';
 
+/**
+ * Token managing instance creation
+ */
 export type Token<I = unknown> = Ref<I>;
 export type SyncToken<I = unknown> = SyncRef<I>;
 export type AsyncToken<I = unknown> = AsyncRef<I>;
