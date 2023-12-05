3a552fbb84f4e4e0b056f4a5123ffd44acd43037
Julien Capellari <julien.capellari@gmail.com>
2023-12-06 00:12:13 +0100

    feat: add scope ref

--- src/defs/scope.ts
+++ src/defs/scope.ts
@@ -1,14 +1,19 @@
+import { Mutable, Readable } from 'kyrielle';
+
 import { Token } from './token.js';
 
 // Types
+export interface ScopeRef<T> extends Readable<T | undefined>, Mutable<T, T> {}
+
 export interface InjectorScope {
   // Attributes
   readonly name: string;
   readonly parent: InjectorScope | null;
 
   // Methods
-  get<T>(token: Token<T>): T | null;
-  set<T>(token: Token<T>, obj: T, global?: boolean): void;
+  get<T>(token: Token<T>): T | undefined;
+  set<T>(token: Token<T>, obj: T): void;
+  ref<T>(token: Token<T>): ScopeRef<T>;
 }
 
 export interface ActiveScope extends Disposable, InjectorScope {
